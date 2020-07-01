d25f57e6863884c2ea367ad7978fbdec541085e3
NguyenVietTrong <nguyenviettrong2004@gmail.com>
2020-07-01 22:20:19 +0700

    initial commit

---
+++ src/constants/todoList.js
@@ -0,0 +1,28 @@
+const todoList = [
+	{
+		id: 1,
+		name: 'Học React JS 1',
+		progress: true
+	},
+	{
+		id: 2,
+		name: 'Học Node JS',
+		progress: true
+	},
+	{
+		id: 3,
+		name: 'Học PHP',
+		progress: true
+	},
+	{
+		id: 4,
+		name: 'Học Bootstrap 4',
+		progress: true
+	},
+	{
+		id: 5,
+		name: 'Học Java Scripts',
+		progress: true
+	},
+];
+export default todoList
